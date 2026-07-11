e3f63c5a2478e020eb3cd77d1dc89c8abd001348
dindogamilo <gamilodindo017@gmail.com>
2026-07-11 14:06:43 +0800

    add initial enums and entities

---
+++ FileProcessor/FileProcessor.Domain/Entities/ProcessedFileDetails.cs
@@ -0,0 +1,22 @@
+﻿using FileProcessor.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileProcessor.Domain.Entities
+{
+    public class ProcessedFileDetails
+    {
+        public Guid Id { get; set; }
+        public string Filename { get; set; } = string.Empty;
+        public FileTypeEnum FileType { get; set; }
+        public string ContentType { get; set; } = string.Empty;
+        public double FileSize { get; set; }
+        public string Content { get; set; }
+        public int RecordCount { get; set; }
+        public double ProcessingTime { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+    }
+}
